0fb063b2796d34be667791e7d32c7006035a52c0
Fouad AMANSOUR <fd.amansour@gmail.com>
2020-08-13 23:16:58 +0200

    middle button is detected

--- main.py
+++ main.py
@@ -1,31 +1,83 @@
 from tkinter import *
-     
-def showPosEvent(event):
-    print ('Widget=%s X=%s Y=%s' % (event.widget, event.x, event.y))
-     
-def showAllEvent(event):
-    print (event)
-    for attr in dir(event): 
-        print (attr, '=>'), getattr(event, attr) 
-     
-     
-def onMiddleClick(event):
-    print ('Got middle mouse button click:'), 
-    showPosEvent(event)
-    showAllEvent(event)
-     
-     
-     
-     
-tkroot = Tk()
-labelfont = ('courier', 20, 'bold')                
-widget = Label(tkroot, text='Hello bind world')
-widget.config(bg='red', font=labelfont)            
-widget.config(height=5, width=20)                  
-widget.pack(expand=YES, fill=BOTH)
+import time
 
-widget.bind('<Button-2>',  onMiddleClick)          
+class StopWatch(Frame):  
+    """ Implements a stop watch frame widget. """                                                                
+    def __init__(self, parent=None, **kw):        
+        Frame.__init__(self, parent, kw)
+        self._start = 0.0        
+        self._elapsedtime = 0.0
+        self._running = 0
+        self.timestr = StringVar()               
+        self.makeWidgets()      
 
-widget.focus()                                     
-tkroot.title('Click Me')
-tkroot.mainloop()
+    def makeWidgets(self):                         
+        """ Make the time label. """
+        l = Label(self, textvariable=self.timestr)
+        self._setTime(self._elapsedtime)
+        l.pack(fill=X, expand=NO, pady=2, padx=2)                      
+    
+    def _update(self): 
+        """ Update the label with elapsed time. """
+        self._elapsedtime = time.time() - self._start
+        self._setTime(self._elapsedtime)
+        self._timer = self.after(50, self._update)
+    
+    def _setTime(self, elap):
+        """ Set the time string to Minutes:Seconds:Hundreths """
+        minutes = int(elap/60)
+        seconds = int(elap - minutes*60.0)
+        hseconds = int((elap - minutes*60.0 - seconds)*100)                
+        self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
+        
+    def Start(self):                                                     
+        """ Start the stopwatch, ignore if running. """
+        if not self._running:            
+            self._start = time.time() - self._elapsedtime
+            self._update()
+            self._running = 1        
+    
+    def Stop(self):                                    
+        """ Stop the stopwatch, ignore if stopped. """
+        if self._running:
+            self.after_cancel(self._timer)            
+            self._elapsedtime = time.time() - self._start    
+            self._setTime(self._elapsedtime)
+            self._running = 0
+    
+    def Reset(self):                                  
+        """ Reset the stopwatch. """
+        self._start = time.time()         
+        self._elapsedtime = 0.0    
+        self._setTime(self._elapsedtime)
+        
+
+        
+def main():
+    root = Tk()
+    sw = StopWatch(root)
+    sw.pack(side=TOP)
+    
+    iLoop=0
+    
+    Button(root, text='Start', command=sw.Start).pack(side=LEFT)
+    Button(root, text='Stop', command=sw.Stop).pack(side=LEFT)
+    Button(root, text='Reset', command=sw.Reset).pack(side=LEFT)
+    Button(root, text='Quit', command=root.quit).pack(side=LEFT)
+    def callback(event):
+        nonlocal iLoop
+        print ("clicked at", event.x, event.y) 
+        if (iLoop % 2)== 0:
+            sw.Start()
+        else:  
+            sw.Stop()
+        iLoop+=1
+    
+    frame = Frame(root, width=100, height=100)
+    frame.bind("<Button-2>", callback)
+    frame.pack()
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
